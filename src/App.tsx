07545be06878937bee85d2af027713c4ae5fb730
hatoba29 <hatoba29@naver.com>
2021-05-30 17:42:17 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+import { render } from "react-dom"
+
+function Main() {
+  return (
+    <>
+      <h1>hello, world!</h1>
+    </>
+  )
+}
+
+render(<Main />, document.getElementById("root"))
